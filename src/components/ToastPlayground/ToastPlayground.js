e7f41d4ce663ef8d9ed5116193fb50a076182743
Timothy Zimmermann <tzimmermann@verity.net>
2025-10-09 15:02:39 +0200

    Squashed commit of the following:
    commit c4029e7982258655e6595a86f147624bff93716b
    Author: Timothy Zimmermann <tzimmermann@verity.net>
    Date:   Thu Oct 9 15:02:28 2025 +0200
    
        mob next [ci-skip] [ci skip] [skip ci]
    
        lastFile:src/components/Toast/Toast.js
    
    commit 2ea88ced750775668431778aafca29274aa3ca77
    Author: Yuanzhen Dai <ydai@verity.net>
    Date:   Thu Oct 9 14:50:00 2025 +0200
    
        mob next [ci-skip] [ci skip] [skip ci]
    
        lastFile:src/components/ToastPlayground/ToastPlayground.js
    
    Co-authored-by: Yuanzhen Dai <ydai@verity.net>

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -1,17 +1,20 @@
-import React from 'react';
+import React from "react";
 
-import Button from '../Button';
+import Button from "../Button";
 
-import styles from './ToastPlayground.module.css';
+import styles from "./ToastPlayground.module.css";
+import Toast from "../Toast/Toast";
 
-const VARIANT_OPTIONS = ['notice', 'warning', 'success', 'error'];
-
+const VARIANT_OPTIONS = ["notice", "warning", "success", "error"];
 
 function ToastPlayground() {
-  const [selectedVariant, setSelectedVariant] = React.useState(VARIANT_OPTIONS[0]);
+  const [selectedVariant, setSelectedVariant] = React.useState(
+    VARIANT_OPTIONS[0]
+  );
   const [message, setMessage] = React.useState("");
+  const [isToastOpen, setIsToastOpen] = React.useState(true);
 
-  console.log(message)
+  console.log(message);
 
   return (
     <div className={styles.wrapper}>
@@ -20,47 +23,56 @@ function ToastPlayground() {
         <h1>Toast Playground</h1>
       </header>
 
+      {isToastOpen && (
+        <Toast variant={selectedVariant} onClose={() => setIsToastOpen(false)}>
+          {message}
+        </Toast>
+      )}
+
       <div className={styles.controlsWrapper}>
         <div className={styles.row}>
           <label
             htmlFor="message"
             className={styles.label}
-            style={{ alignSelf: 'baseline' }}
+            style={{ alignSelf: "baseline" }}
           >
             Message
           </label>
           <div className={styles.inputWrapper}>
-            <textarea id="message" className={styles.messageInput} value={message} onChange={(event) => setMessage(event.target.value)} />
+            <textarea
+              id="message"
+              className={styles.messageInput}
+              value={message}
+              onChange={(event) => setMessage(event.target.value)}
+            />
           </div>
         </div>
 
         <div className={styles.row}>
           <div className={styles.label}>Variant</div>
-          <div
-            className={`${styles.inputWrapper} ${styles.radioWrapper}`}
-          >
-            {VARIANT_OPTIONS.map(option => <label htmlFor={`variant-${option}`} key={option}>
-              <input
-                id={`variant-${option}`}
-                type="radio"
-                name="variant"
-                value={option}
-                checked={selectedVariant === option}
-                onChange={event => {
-                  setSelectedVariant(event.target.value)
-                }}
-              />
-              {option}
-            </label>)}
+          <div className={`${styles.inputWrapper} ${styles.radioWrapper}`}>
+            {VARIANT_OPTIONS.map((option) => (
+              <label htmlFor={`variant-${option}`} key={option}>
+                <input
+                  id={`variant-${option}`}
+                  type="radio"
+                  name="variant"
+                  value={option}
+                  checked={selectedVariant === option}
+                  onChange={(event) => {
+                    setSelectedVariant(event.target.value);
+                  }}
+                />
+                {option}
+              </label>
+            ))}
           </div>
         </div>
 
         <div className={styles.row}>
           <div className={styles.label} />
-          <div
-            className={`${styles.inputWrapper} ${styles.radioWrapper}`}
-          >
-            <Button>Pop Toast!</Button>
+          <div className={`${styles.inputWrapper} ${styles.radioWrapper}`}>
+            <Button onClick={() => setIsToastOpen(true)}>Pop Toast!</Button>
           </div>
         </div>
       </div>
